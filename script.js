5ebebcd4ad631ada4d98306563aaf9058bddc28d
yeyTaken <99534446+yeyTaken@users.noreply.github.com>
2023-04-29 23:11:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ infoButton.addEventListener('click', function() {
       infoPanel.style.display = 'block';
     }
   });
+
+  //============
+  function aceitarCookies() {
+    // define o cookie com uma validade de 30 dias
+    var data = new Date();
+    data.setTime(data.getTime() + (30 * 24 * 60 * 60 * 1000));
+    var expires = "expires="+ data.toUTCString();
+    document.cookie = "cookiesAceitos=true;" + expires + ";path=/";
+    // remove o aviso de cookies
+    document.getElementById("aviso-de-cookies").style.display = "none";
+  }
+  
